8de8ed28658f567530726c182efdd676542af97f
hachiko3000 <67519851+hachiko3000@users.noreply.github.com>
2020-10-13 22:01:14 -0500

    Create FindIndex.js

---
+++ FindIndex.js
@@ -0,0 +1,14 @@
+function isPrime(element, index, array) {
+  var start = 2;
+  while (start <= Math.sqrt(element)) {
+    if (element % start < 1) {
+    return false;
+    } else {
+     start++;
+     }
+     }
+     return element > 1;
+     }
+     
+     console.log([4, 6, 8, 12].findIndex(isPrime)); // -1
+     console.log([4, 6, 7, 12].findIndex(isPrime)); // 2 
