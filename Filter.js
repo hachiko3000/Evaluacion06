ccdafbe21b26e8cf8064af9f38c06e01d0976760
hachiko3000 <67519851+hachiko3000@users.noreply.github.com>
2020-10-13 21:58:59 -0500

    Create Filter.js

---
+++ Filter.js
@@ -0,0 +1,9 @@
+myFilter = (array, callback) => {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
