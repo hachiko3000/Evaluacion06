96ea536a03f1f25f10a8f276f80343a7b6075f72
hachiko3000 <67519851+hachiko3000@users.noreply.github.com>
2020-10-13 21:59:59 -0500

    Create Map.js

---
+++ Map.js
@@ -0,0 +1,12 @@
+var numbers = [1, 5, 10, 15];
+var doubles = numbers.map(function(x) {
+});
+
+// doubles es ahora [2, 10, 20, 30]
+// numbers permanece [1, 5, 10, 15]
+
+var numbers = [1, 4, 9];
+var roots = numbers.map(function(num) {
+  return.Math.sqrt(num);
+  });
+  
